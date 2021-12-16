70aa763e1d38cc6c9185f9f6ab60414baaf4f248
Israel Brewster <ijbrewster@alaska.edu>
2021-12-16 08:05:51 -0900

    Add omps processing files to repository

---
+++ genOmps.py
@@ -0,0 +1,37 @@
+#!/tropomi_download/bin/python
+import logging
+import sys
+import argparse
+from datetime import datetime
+from dateutil.parser import parse
+
+
+sys.path.append('/tropomi_download/')
+from VolcView import main as genVolcView
+
+LOG_FILE = '/var/log/omps.log'
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError(f'Boolean value expected. (got: {v})')
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Generate a volcview image and upload')
+    parser.add_argument('file',type=str)
+    parser.add_argument('spawn',type=str2bool, default=True, nargs='?')
+
+    args=parser.parse_args()
+    file_name=args.file
+    use_spawn=args.spawn
+    logging.info("Running with args %s, %s",file_name,str(use_spawn))
+    genVolcView(file_name,use_spawn)
+    logging.info("Volc view generation completed")
+    sys.exit(0)
+    print("Called sys.exit")
