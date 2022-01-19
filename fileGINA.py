35bd7594aac09e1dc76aa9f494878dc4c8b87a85
Israel Brewster <ijbrewster@alaska.edu>
2022-01-19 12:31:25 -0900

    Update fileGINA for incrond

--- fileGINA.py
+++ fileGINA.py
@@ -1,10 +1,8 @@
 #!/tropomi_download/bin/python
-import json
 import logging
 import os
 import sys
 from datetime import datetime, timezone
-from concurrent.futures import ProcessPoolExecutor
 
 from VolcView import main as genVolcView
 
@@ -19,47 +17,38 @@ logging.basicConfig(filename=LOG_FILE,
                     )
 
 if __name__ == "__main__":
-    args = sys.argv
-    print(args)
+    files = sys.argv[1:]
 
-    logging.info("Got file_info of: %s", str(file_infos))
-    gen_procs = []
-    with ProcessPoolExecutor() as executor:
-        for file in file_infos:
-            file_name = file['name']
+    logging.info("Got file_info of: %s", str(files))
+    for file in files:
+        file_name = os.path.basename(file)
 
-            if not file_name.endswith('.h5') or not file['exists']:
-                continue
+        if not file_name.endswith('.h5') or not file['exists']:
+            continue
 
-            if file_name.startswith('V'):
-                # VIIRS file
-                date_part = file_name[1:14]
-                date_format = '%Y%j%H%M%S'
-                DEST_PATH = '/data/viirs'
-            else:
-                # OMPS
-                file_parts = file_name.split('_')
-                date_part = file_parts[3]
-                date_format = '%Ym%m%dt%H%M%S'
-                DEST_PATH = '/omps_data'
+        if file_name.startswith('V'):
+            # VIIRS file
+            date_part = file_name[1:14]
+            date_format = '%Y%j%H%M%S'
+            DEST_PATH = '/data/viirs'
+        else:
+            # OMPS
+            file_parts = file_name.split('_')
+            date_part = file_parts[3]
+            date_format = '%Ym%m%dt%H%M%S'
+            DEST_PATH = '/omps_data'
 
-            file_time = datetime.strptime(date_part, date_format)
-            file_time = file_time.replace(tzinfo=timezone.utc)
-            formatted_date = file_time.strftime('%Y-%m-%d')
+        file_time = datetime.strptime(date_part, date_format)
+        file_time = file_time.replace(tzinfo=timezone.utc)
+        formatted_date = file_time.strftime('%Y-%m-%d')
 
-            os.makedirs(f"{DEST_PATH}/{formatted_date}", exist_ok=True)
-            dest_file = f"{DEST_PATH}/{formatted_date}/{file_name}"
-            os.rename(f"{SRC_PATH}/{file_name}", dest_file)
-            logging.info("Filed: %s %s", file_name, formatted_date)
+        os.makedirs(f"{DEST_PATH}/{formatted_date}", exist_ok=True)
+        dest_file = f"{DEST_PATH}/{formatted_date}/{file_name}"
+        os.rename(f"{SRC_PATH}/{file_name}", dest_file)
+        logging.info("Filed: %s %s", file_name, formatted_date)
 
-            logging.info("Generating volc view images")
-            future = executor.submit(genVolcView, dest_file)
-            gen_procs.append(future)
-
-    logging.info("Waiting for process complete")
-    for idx, future in enumerate(gen_procs):
-        logging.info("Process %i of %i complete with result: %s", idx + 1,
-                     len(gen_procs), str(future.result()))
+        logging.info("Generating volc view images")
+        genVolcView(dest_file)
 
     logging.info("Complete")
     with open('/tmp/watchmanPython.log', 'a') as logfile:
