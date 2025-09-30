f7f51c39ffc63b1d4301e890479577d9647c82cd
Israel Brewster <ijbrewster@alaska.edu>
2025-09-30 10:11:38 -0800

    Completly remove parallelisim from processing code - rate that it would be needed anyway.

--- processGINA.py
+++ processGINA.py
@@ -10,13 +10,9 @@ import logging
 import os
 import shutil
 
-from concurrent.futures import ProcessPoolExecutor, process
 from datetime import datetime, timezone
-from functools import partial
 
 import paho.mqtt.client as mqtt
-
-executor: ProcessPoolExecutor = None
 
 logging.basicConfig(filename=ginaConfig.LOG_FILE,
                     level=logging.INFO,
@@ -25,19 +21,6 @@ logging.basicConfig(filename=ginaConfig.LOG_FILE,
                     )
 
 SRC_PATH = '/gina_root/upload'
-
-def future_complete(filename, future):
-    try:
-        result = future.result()
-    except process.BrokenProcessPool:
-        logging.error("!!!!!!!!Process pool died. Re-creating")
-        create_process_pool()
-        result="Process Pool Restart"
-    except Exception:
-        logging.exception("An exception occurred while processing file")
-        result = future
-
-    logging.info(f"Completed processing of {filename} with return value {result}")
 
 
 def on_message(client, userdata, message):
@@ -93,18 +76,11 @@ def on_message(client, userdata, message):
         logging.info("Filed: %s %s", file_name, formatted_date)
 
         logging.info("Generating volc view images")
-
-        # Fire and forget
-        complete_callback = partial(future_complete, file_name)
         try:
-            future = executor.submit(genVolcView, dest_file, False)
-        except process.BrokenProcessPool:
-            logging.exception("Process pool broken. Creating a new one and trying again.")
-            create_process_pool()
-            future = executor.submit(genVolcView, dest_file, False)
-
-        future.add_done_callback(complete_callback)
-        # genVolcView(dest_file, False)
+            result = genVolcView(dest_file)
+            logging.info(f"Completed processing of {file_name} with return value {result}")
+        except Exception:
+            logging.exception(f"An exception occured while processing {file_name}")
     except Exception:
         logging.exception("Unable to process message")
         
@@ -112,18 +88,6 @@ def on_connect(client: mqtt.Client, userdata, flags, rc):
     logging.info(f"Connected to MQTT broker with result code {rc}")
     client.subscribe('GINA', qos=2)
     logging.info("Subscribed to GINA topic")
-
-def create_process_pool():
-    global executor
-    old_executor = executor
-    spawn_context = multiprocessing.get_context('spawn')
-    executor = ProcessPoolExecutor(max_workers=2, max_tasks_per_child = 1, mp_context = spawn_context)
-    
-    if old_executor:
-        try:
-            old_executor.shutdown(wait=False, cancel_futures=False)
-        except Exception:
-            logging.exception("Error shutting down old executor")
             
 def on_disconnect(client, userdata, rc):
     if rc != 0:
@@ -132,14 +96,11 @@ def on_disconnect(client, userdata, rc):
         logging.info("MQTT disconnected cleanly")
 
 if __name__ == "__main__":
-    create_process_pool()
-
     client = mqtt.Client(client_id="gina_processing", clean_session=False)
     client.on_message = on_message
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.connect(ginaConfig.MQTT_SERVER)    
-    # client.subscribe('GINA')
     
     try:
         client.loop_forever(retry_first_connection = True)
@@ -147,6 +108,4 @@ if __name__ == "__main__":
         logging.info("Shutting down...")
     finally:
         client.disconnect()
-        if executor:
-            executor.shutdown(wait=True, cancel_futures=False)
         logging.info("Shutdown complete")
